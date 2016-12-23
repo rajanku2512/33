ff554654674d7edfa4f3330a2931ccb2e0607629
rajanku2512 <rajanku2512@gmail.com>
2016-12-23 20:57:02 +0530

    Add files via upload

---
+++ Assignment33.java
@@ -0,0 +1,9 @@
+
+public class Assignment33 {int x; int y;
+String name;
+public static void main(String args[]) {
+example pnt = new example();
+System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+} }
+
+}
